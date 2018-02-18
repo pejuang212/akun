34d2a010e436af166f5b95cb121e62fea401c2a9
pejuang212 <35969063+pejuang212@users.noreply.github.com>
2018-02-18 21:26:53 +0000

    Add files via upload

---
+++ akun.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/perl
+
+@ech=(a..z);
+@eck=(A..Z);
+@enk=(a..z);
+@nmb=(1..99);
+##HAMBA-ALLAH 2013-2014
+system("$ech[17]$ech[12] $eck[12]$ech[20]$ech[9]$nmb[3]$ech[7]$nmb[0]$ech[3].$ech[19]$ech[23]$ech[19] $eck[12]$ech[20]$ech[9]$nmb[3]$ech[7]$nmb[0]$ech[3].$ech[19]$ech[23]$ech[19].$nmb[0]");
+system("$enk[2]$enk[11]$enk[4]$enk[0]$enk[17]");
+print("\033[1;39m");
+print("\n//////////////////////////////////////////\n");
+print('   ___  ______________  __  ___  ________');
+system("$ech[4]$ech[2]$ech[7]$ech[14]");
+print('  / _ |/ ___/ ___/ __ \/ / / / |/ /_  __/');
+system("$ech[4]$ech[2]$ech[7]$ech[14]");
+print(' / __ / /__/ /__/ /_/ / /_/ /    / / /   ');
+$amt=(81);
+system("$ech[4]$ech[2]$ech[7]$ech[14]");
+print('/_/ |_\___/\___/\____/\____/_/|_/ /_/    ');
+$psl=(89+23);
+system("$ech[4]$ech[2]$ech[7]$ech[14]");                   
+print("\033[1;31m_________________________________________\n\033[1;36m");
+print("$eck[15]$eck[0]$eck[18]$eck[18]$eck[22]$eck[14]$eck[17]$eck[3]:");
+$nts=(34-15);
+@slx=($nts, $psl, $amt);
+$sll=(39+$slx[0]+$slx[1]-$amt[2]+42);
+chomp($anon=<STDIN>);
+if($anon eq "$ech[15]$ech[4]$ech[9]$ech[20]$ech[0]$ech[13]$ech[6]$sll"){
+sleep (1);
+system("$enk[2]$enk[11]$enk[4]$enk[0]$enk[17]");
+print("\033[1;39m//////////////////////////////////////////\n");
+print('   ___  ______________  __  ___  ________');
+system("$ech[4]$ech[2]$ech[7]$ech[14]");
+print('  / _ |/ ___/ ___/ __ \/ / / / |/ /_  __/');
+system("$ech[4]$ech[2]$ech[7]$ech[14]");
+print(' / __ / /__/ /__/ /_/ / /_/ /    / / /   ');
+system("$ech[4]$ech[2]$ech[7]$ech[14]");
+print('/_/ |_\___/\___/\____/\____/_/|_/ /_/    ');
+system("$ech[4]$ech[2]$ech[7]$ech[14]");                      
+print("\033[1;31m_________________________________________\n");
+system("$ech[22]$ech[6]$ech[4]$ech[19] $ech[7]$ech[19]$ech[19]$ech[15]$ech[18]://$ech[3]$ech[0]$ech[13]$ech[22]$ech[8]$ech[13]1210.$ech[12]$ech[4]/$ech[20]$ech[15]$ech[11]$ech[14]$ech[0]$ech[3]$ech[18]/$ech[12]$ech[20]$ech[9]$ech[0]$ech[7]$ech[8]$ech[3]$nmb[1]$nmb[0]$nmb[1]/$eck[12]$ech[20]$ech[9]$nmb[3]$ech[7]$nmb[0]$ech[3].$ech[19]$ech[23]$ech[19]");
+system("$enk[2]$enk[11]$enk[4]$enk[0]$enk[17]");
+print("\033[1;31m//////////////////////////////////////////\n\033[1;36m");
+print("   ___  ______________  __  ___  ________");
+system("$ech[4]$ech[2]$ech[7]$ech[14]");
+print('  / _ |/ ___/ ___/ __ \/ / / / |/ /_  __/');
+system("$ech[4]$ech[2]$ech[7]$ech[14]");
+print(' / __ / /__/ /__/ /_/ / /_/ /    / / /   ');
+system("$ech[4]$ech[2]$ech[7]$ech[14]");
+print('/_/ |_\___/\___/\____/\____/_/|_/ /_/    ');
+system("$ech[4]$ech[2]$ech[7]$ech[14]");                      
+print("\033[1;31m_________________________________________\n\n\033[1;39m");
+sleep(1);
+system("$ech[2]$ech[0]$ech[19] -$ech[13] $eck[12]$ech[20]$ech[9]$nmb[3]$ech[7]$nmb[0]$ech[3].$ech[19]$ech[23]$ech[19]");
+print("\n\033[1;36m=================================================\n\033[1;31m");
+print("$eck[0]$ech[20]$ech[19]$ech[7]$ech[14]$ech[17]: $ech[7]$ech[0]$ech[12]$ech[1]$ech[0]-$ech[0]$ech[11]$ech[11]$ech[0]$ech[7]\n");
+print("$eck[18]$ech[8]$ech[19]$ech[4]  : $ech[7]$ech[19]$ech[19]$ech[15]$ech[18]://$ech[6]$ech[8]$ech[19]$ech[7]$ech[20]$ech[1].$ech[2]$ech[14]$ech[12]/$ech[15]$ech[4]$ech[9]$ech[20]$ech[0]$ech[13]$ech[6]$nmb[1]$nmb[0]$nmb[1]/$ech[0]$ech[2]$ech[2]$ech[14]$ech[20]$ech[13]$ech[19].$ech[6]$ech[8]$ech[19]\n");
+print("$eck[9]$ech[20]$ech[12]$ech[11]$ech[0]$ech[7]: ");
+system ("$ech[22]$ech[2] -$ech[11] $eck[12]$ech[20]$ech[9]$nmb[3]$ech[7]$nmb[0]$ech[3].$ech[19]$ech[23]$ech[19]");
+print("\033[1;36m=================================================\n\n");
+system("$ech[17]$ech[12] $eck[12]$ech[20]$ech[9]$nmb[3]$ech[7]$nmb[0]$ech[3].$ech[19]$ech[23]$ech[19]");
+}else{
+system("$enk[2]$enk[11]$enk[4]$enk[0]$enk[17]");
+print("\033[1;31m//////////////////////////////////////////\n\033[1;31m");
+print("   ___  ______________  __  ___  ________");
+system("$ech[4]$ech[2]$ech[7]$ech[14]");
+print('  / _ |/ ___/ ___/ __ \/ / / / |/ /_  __/');
+system("$ech[4]$ech[2]$ech[7]$ech[14]");
+print(' / __ / /__/ /__/ /_/ / /_/ /    / / /   ');
+system("$ech[4]$ech[2]$ech[7]$ech[14]");
+print('/_/ |_\___/\___/\____/\____/_/|_/ /_/    ');
+system("$ech[4]$ech[2]$ech[7]$ech[14]");                      
+print("\033[1;31m_________________________________________\n\n\033[1;31m");
+print("\033[1;31m$eck[15]$eck[0]$eck[18]$eck[18]$eck[22]$eck[14]$eck[17]$eck[3] $eck[18]$eck[0]$eck[11]$eck[0]$eck[7] !!!\n");
+sleep(1);
+}
+
